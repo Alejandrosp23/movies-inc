dfaa73569f9e25ba915c05514b1e41bfa6e3ef5b
Alejandrosp23 <alejandrosantos1123@hotmail.com>
2025-01-07 00:25:30 -0400

    fix: Appbar title and UI

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTitleAlign: 'center',
+      }}>
+      {/* Optionally configure static options outside the route.*/}
+      <Stack.Screen 
+        name="index"
+        options={{
+          title: 'Movies',
+        }} />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            title: 'Movie Details',
+          }} />
+    </Stack>
+  );
 }
